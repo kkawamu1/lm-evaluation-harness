2b8c320cfd16ee29ac4095a19472a7fb6f7cac8a
Samson Tan <samsontmr@users.noreply.github.com>
2022-06-17 20:10:20 -0700

    add secondary task

--- lm_eval/tasks/tydiqa.py
+++ lm_eval/tasks/tydiqa.py
@@ -41,7 +41,7 @@ class TyDiQAPrimary(PromptSourceTask):
         return self.dataset["validation"]
 
     def invalid_doc_for_prompt(self, doc) -> bool:
-        # HACK: Some copa templates have conditionals that ignore documents
+        # HACK: Some templates have conditionals that ignore documents
         # when the condition is not met, like `{if doc['question'] != \"cause\"}`.
         # This means the prompt will never produce an input and target.
         # TODO: Remove this when fixed in `promptsource`
@@ -50,3 +50,69 @@ class TyDiQAPrimary(PromptSourceTask):
             return False
         except:
             return True
+        
+    class TyDiQASecondary(PromptSourceTask):
+    VERSION = 1
+    DATASET_PATH = "tydiqa"
+    DATASET_NAME = "secondary_task"
+
+    def has_training_docs(self):
+        return True
+
+    def has_validation_docs(self):
+        return True
+
+    def has_test_docs(self):
+        return False
+
+    def training_docs(self):
+        if self._training_docs is None:
+            self._training_docs = list(self.dataset["train"])
+        return self._training_docs
+
+    def validation_docs(self):
+        return self.dataset["validation"]
+
+    def max_generation_length(self):
+        return 128
+
+    def compute_score(self, targets, pred, metric=compute_exact, agg=max):
+        return agg([metric(t, pred) for t in targets])
+
+    def process_results(self, doc, results):
+        # Based on PIAF's implementation
+        targets = self.doc_to_target(doc)
+        pred = results[0].strip()
+        agg_exact_match = self.compute_score(targets, pred, compute_exact)
+        agg_f1 = self.compute_score(targets, pred, compute_f1)
+        
+        out = {"f1": agg_f1, 
+                "exact_match": agg_exact_match}
+
+        if self.save_examples:
+            example = {"target": targets, "pred": pred}
+            return out, example
+        return out
+
+    def invalid_doc_for_prompt(self, doc) -> bool:
+        # HACK: Some templates have conditionals that ignore documents
+        # when the condition is not met, like `{if doc['question'] != \"cause\"}`.
+        # This means the prompt will never produce an input and target.
+        # TODO: Remove this when fixed in `promptsource`
+        try:
+            self.prompt.apply(doc)
+            return False
+        except:
+            return True
+    
+    def higher_is_better(self):
+        return {
+            "f1": True,
+            "exact_match": True,
+        }
+
+    def aggregation(self):
+        return {
+            "f1": mean,
+            "exact_match": mean,
+        }
